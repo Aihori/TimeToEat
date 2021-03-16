f039e41f7bf14d1b4dc16d1843ef56025e001a38
Aihori <77650114+Aihori@users.noreply.github.com>
2021-03-16 01:35:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+async function handle(event){
+    console.log("A form has arrived!");
+    $("#emotion").html("Loading...");
+    event.preventDefault();
 
+    var myForm = document.getElementById("image-form");
+    var payload = new FormData(myForm);
+
+    var functionUrl = "https://timetoeat.azurewebsites.net/api/HttpTrigger1?code=zLD/jBqIBGhLEk1wP5aeJgexJtPXFefUHasxWp7pjTNPvHt10iVnag=="
+
+    const resp = await fetch(functionUrl, {
+        method: "POST",
+        body: payload,
+    });
+
+    var data = await resp.json();
